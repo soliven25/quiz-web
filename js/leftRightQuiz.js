9a06e5db4f09f365d545f0a4358511cb764c1b9b
Sungkeun Choi <soliven25@gmail.com>
2016-04-30 11:18:59 +0900

    change logic to meet the number of choice & display

--- js/leftRightQuiz.js
+++ js/leftRightQuiz.js
@@ -1,54 +1,52 @@
 window.onload = function(){
-
-            var leftScore = 0;    /* score for left brain */
-            var rightScore = 0;   /* schore for right brain */
-            var numberOfChoice = 1;  /* progressed quiz num */
-            var leftArray = ["default","개가 좋아요!","쥐가 좋아요!","소가 좋아요!"];
-            var rightArray = ["default","고양이가 좋아요!", "햄스터가 좋아요!", "양이 좋아요!"];
+    var leftScore = 0;    /* score for left brain */
+    var rightScore = 0;   /* schore for right brain */
+    var numberOfChoice = 1;  /* progressed quiz num */
+    var leftArray = ["default","개가 좋아요!","쥐가 좋아요!","소가 좋아요!"];
+    var rightArray = ["default","고양이가 좋아요!", "햄스터가 좋아요!", "양이 좋아요!"];
             
-            var leftChoice = document.getElementById("choiceone"); /* DOM element for left */
-            var rightChoice = document.getElementById("choicetwo"); /* DOM element for right */
-            var remainDisplay = document.getElementById("remainChoice");
-            var resultDisplay = document.getElementById("result");
+    var leftChoice = document.getElementById("choiceone"); /* DOM element for left */
+    var rightChoice = document.getElementById("choicetwo"); /* DOM element for right */
+    var remainDisplay = document.getElementById("remainChoice");
+    var resultDisplay = document.getElementById("result");
             
-            function displayResult(){
-                 if(leftScore > rightScore)
-                    resultDisplay.innerHTML = "당신은 좌뇌형이네요~~"; 
-                 else
-                    resultDisplay.innerHTML = "당신은 우뇌형이네요~~";               
-            }
+    function displayResult(){
+        if(leftScore > rightScore)
+            resultDisplay.innerHTML = "당신은 좌뇌형이네요~~"; 
+        else
+            resultDisplay.innerHTML = "당신은 우뇌형이네요~~";               
+    }
             
-            leftChoice.onclick = function(){
+    leftChoice.onclick = function(){
+        if(numberOfChoice > 20)
+            return;
+                  
+        numberOfChoice++;
+        leftScore++;
+        rightChoice.value = rightArray[numberOfChoice];              
+        leftChoice.value = leftArray[numberOfChoice];
                 
-                if(numberOfChoice > 20)
-                    return;
-                    
-                leftScore++;
-                rightChoice.value = rightArray[numberOfChoice];              
-                leftChoice.value = leftArray[numberOfChoice];
-                
-                if(20 === numberOfChoice){
-                    displayResult();
-                }     
+        if(21 === numberOfChoice){
+            displayResult();
+        }else{
+            remainDisplay.innerHTML = numberOfChoice + "/20";
+        }     
+    }
                       
-                numberOfChoice++;
-                remainDisplay.innerHTML = numberOfChoice + "/20";
-            }
-                      
-            rightChoice.onclick = function(){
-                if(numberOfChoice > 20)
-                    return;
+    rightChoice.onclick = function(){
+        if(numberOfChoice > 20)
+            return;
                     
-                rightScore++;
-                leftChoice.value = leftArray[numberOfChoice];
-                rightChoice.value = rightArray[numberOfChoice];
+        numberOfChoice++;
+        rightScore++;
+        leftChoice.value = leftArray[numberOfChoice];
+        rightChoice.value = rightArray[numberOfChoice];
                 
-                if(20 === numberOfChoice){
-                    displayResult();
-                }
-                
-                numberOfChoice++;
-                remainDisplay.innerHTML = numberOfChoice +"/20";                                                          
-            }
+        if(21 === numberOfChoice){
+            displayResult();
+        }else{
+            remainDisplay.innerHTML = numberOfChoice +"/20";   
+        }                                                                                           
+    }
             
 }
